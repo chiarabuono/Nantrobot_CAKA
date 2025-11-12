3a52a7b8726bb2ed9b1343a787de76bcd9a20623
chiarabuono <s7687956@studenti.unige.it>
2025-11-12 19:41:40 +0100

    introduction of avoidance function and compute distance from target

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <math.h>
 
 #define EncoderPin1A 22
 #define EncoderPin1B 23
@@ -10,11 +11,42 @@
 #define motorLforward 16
 #define motorLbackward 17
 
+float const pi = 3.141592653589793;
+int avoidance_state = 0;
+
+int state = 1;
+/*
+  state 0: no obstacles - go straight ahead
+  state 1: obstacles - turning
+  state 2: turned enough, go straight ahead for a second
+  state 3: align again with the target
+*/
+
 // sonar pins
 #define trigPin 0
 #define echoPin 0
 
 float duration, distance;
+float distance_to_move = 0.0;
+float radians_to_turn = 0.0;
+float safe_dist = 15;     // [cm]
+float prev_dist = 0;      // [cm] obstacle from the robot
+
+struct point {
+  float x;
+  float y;
+  float theta;            // theta = 0 then robot moving along positive x
+};
+
+point robot_pos = {0, 0, 0};
+point destination_point = {2, 3, 0};
+
+void stopmotors() {
+  digitalWrite(motorRforward, LOW);
+  digitalWrite(motorRbackward, LOW);
+  digitalWrite(motorLforward, LOW);
+  digitalWrite(motorLbackward, LOW);  
+}
 
 void move_forward() {
   digitalWrite(motorRforward, HIGH);
@@ -44,6 +76,19 @@ void turn_right() {
   digitalWrite(motorLbackward, LOW);
 }
 
+
+float compute_distance(point start_pos, point end_pos) {
+  return std::sqrt(pow((start_pos.x - end_pos.x), 2) + pow((start_pos.y - end_pos.y), 2));
+}
+
+float compute_radians(point start_pos, point end_pos) {
+  float delta_x = start_pos.x - end_pos.x;
+  float delta_y = start_pos.y - end_pos.y;
+  float rel_angle = atan2(delta_y, delta_x);
+
+  return rel_angle - start_pos.theta;
+}
+
 float read_sonar() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -60,6 +105,10 @@ float read_sonar() {
   return distance;
 }
 
+void obstacle_avoidance() {
+
+}
+
 
 void setup() {
   // set up motor pins
@@ -74,16 +123,38 @@ void setup() {
   
   Serial.begin(9600);
 
+  distance_to_move = compute_distance(robot_pos, destination_point);
+  radians_to_turn = compute_radians(robot_pos, destination_point);
+
+}
+
+void go_to_target() {
+
 }
 
 void loop() {
-  /*
-  turn_left();
-  delay(500);
-  turn_right();
-  delay(1000);
-  */
+  float dist = read_sonar();
 
-  //read_sonar();
+  switch (avoidance_state){
+    case 0:
+      if (dist < safe_dist) {
+        avoidance_state = 1;
+      break;
+      }
+      go_to_target();
+      break;
+  
+    case 1:
+      obstacle_avoidance();
+      break;
+  }
+  
 
 }
+
+/*
+  state 0: no obstacles - go straight ahead
+  state 1: obstacles - turning
+  state 2: turned enough, go straight ahead for a second
+  state 3: align with the target
+*/
